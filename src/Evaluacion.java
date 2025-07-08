5b5355d5756d0946067e9f127cb42b03e65e8d52
Osvaldo28th <osvaldoencino@gmail.con>
2025-07-08 16:14:45 -0600

    Implemenacion de botones y ajuste

--- src/Evaluacion.java
+++ src/Evaluacion.java
@@ -3,15 +3,35 @@ import javax.swing.*;
 import java.awt.event.*;
 
 public class Evaluacion extends JFrame{
+    private JButton btnTijera, btnPiedra, btnPapel, btnSalir;
     private int cont;
     
     public Evaluacion(){
         // Caracteristicas de la ventana
         setTitle("Piedra, Papel o Tijera");
-        setSize(600, 600);
+        setSize(480, 600);
         setLayout(null);
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         setLocationRelativeTo(null);
+        
+        crearBotones();
     }
-
+    
+    public void crearBotones(){
+        btnTijera = new JButton("TIJERA");
+        btnTijera.setBounds(30, 450, 90, 30);
+        add(btnTijera);
+        
+        btnPiedra = new JButton("PIEDRA");
+        btnPiedra.setBounds(135, 450, 90, 30);
+        add(btnPiedra);
+        
+        btnPapel = new JButton("PAPEL");
+        btnPapel.setBounds(235, 450, 90, 30);
+        add(btnPapel);
+        
+        btnSalir = new JButton("FINALIZAR");
+        btnSalir.setBounds(340, 450, 100, 30);
+        add(btnSalir);
+    }
 }
