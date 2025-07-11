7b0fc0d9c111e628469590009d9a9777eef14f82
Osvaldo28th <osvaldoencino@gmail.con>
2025-07-10 19:41:57 -0600

    Evaluacion_completada

--- src/Evaluacion.java
+++ src/Evaluacion.java
@@ -6,7 +6,7 @@ import java.awt.event.*;
 
 public class Evaluacion extends JFrame{
     private Opcion usuario;
-    private int cont;
+    private int cont, V, D , E;
     private JButton btnTijera, btnPiedra, btnPapel, btnSalir;
     private JLabel lblInfo, lblCont;
     
@@ -39,6 +39,7 @@ public class Evaluacion extends JFrame{
        btnTijera.addActionListener(new ActionListener(){
            public void actionPerformed(ActionEvent e){
                usuario.setOpcion(1);
+               evaluarJugada();
            }
        });
        
@@ -46,13 +47,59 @@ public class Evaluacion extends JFrame{
         btnPiedra = new JButton("PIEDRA");
         btnPiedra.setBounds(135, 450, 90, 30);
         add(btnPiedra);
+        btnPiedra.addActionListener(new ActionListener(){
+            public void actionPerformed(ActionEvent e){
+                usuario.setOpcion(2);
+                evaluarJugada();
+            }
+        });
         
         btnPapel = new JButton("PAPEL");
         btnPapel.setBounds(235, 450, 90, 30);
         add(btnPapel);
+        btnPapel.addActionListener(new ActionListener(){
+            public void actionPerformed(ActionEvent e){
+                usuario.setOpcion(3);
+                evaluarJugada();
+            }
+        });
         
         btnSalir = new JButton("FINALIZAR");
         btnSalir.setBounds(340, 450, 100, 30);
         add(btnSalir);
+        btnSalir.addActionListener(new ActionListener(){
+            public void actionPerformed(ActionEvent e){
+                usuario.setOpcion(4);
+            }
+        });
+    }
+    
+    public void evaluarJugada(){
+        // Variable que guarda la jugada de la pc
+        int jugPc = (int)(Math.random()*3)+1;
+        
+        // Jugada de la pc
+        if(jugPc==1){
+            JOptionPane.showMessageDialog(null, "Jugada de la PC: Tijera");
+        } else if(jugPc==2){
+            JOptionPane.showMessageDialog(null, "Jugada de la PC: Piedra");
+        } else{
+            JOptionPane.showMessageDialog(null, "Jugada de la PC: Papel");
+        }
+        
+        // Evaluacion de la jugada de la pc y el usuario
+        if(usuario.getOpcion() == jugPc){
+            E++;
+            JOptionPane.showMessageDialog(null, "Empate");
+        } else if((usuario.getOpcion()==1 && jugPc==3) || 
+                  (usuario.getOpcion()==2 && jugPc==1) ||
+                  (usuario.getOpcion()==3 && jugPc==2)){
+            V++;
+            JOptionPane.showMessageDialog(null, "Victoria");
+        } else{
+            D++;
+            JOptionPane.showMessageDialog(null, "Derrota");
+        }   
+        
     }
 }
